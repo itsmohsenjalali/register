f0bc1de3cb0084677d4f2bca5715dc793995536e
mo-0day <34004821+mo-0day@users.noreply.github.com>
2017-12-12 12:57:00 +0330

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -23,8 +23,8 @@ class DBhelper:
         self.conn.commit()
     def add_item(self,item_list):
         cur = self.conn.cursor()
-        cur.execute("""INSERT INTO register1 (ID,NAME,SID) /
-        VALUES (%d,%s,%s)""".format(item_list[0],item_list[1],item_list[2]))
+        cur.execute("""INSERT INTO register1 (ID,NAME,SID)
+        VALUES (%d,%s,%s)""",(item_list[0],item_list[1],item_list[2]))
         self.conn.commit()
     def delete_item(self,ID):
         cur = self.conn.cursor()
